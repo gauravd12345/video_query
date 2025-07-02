aaab5439e2747134a7271a94b10f04bfb0686528
Gaurav <gauravdharmadhikari2@gmail.com>
2025-07-02 13:44:01 -0700

    updated chat interface

--- video_query/src/components/ChatInterface.jsx
+++ video_query/src/components/ChatInterface.jsx
@@ -3,10 +3,10 @@ import Conversation from "./Conversation"
 
 const ChatInterface = () => {
   const [allTexts, setAllTexts] = useState([[1, "What can I help with?"]])
-  const [chatBotTexts, setChatBotTexts] = useState(["What can I help with?"])
+  const [chatBotTexts, setChatBotTexts] = useState([""])
   const [userTexts, setUserTexts] = useState([])
   const [text, setText] = useState("")
-  const [isUser, setIsUser] = useState(false)
+  const [isUser, setIsUser] = useState(true)
 
   return (
     <div className="font-inter w-[360px] h-[500px] box-border border border-white overflow-hidden flex flex-col bg-white dark:bg-neutral-800 rounded-2xl shadow-[0_0_20px_rgba(0,0,0,0.5)]">
@@ -19,23 +19,31 @@ const ChatInterface = () => {
       </div>
 
       <div>
-        <button onClick={() => {setIsUser(!isUser)}}>Toggle: {isUser ? <label>User</label> : <label>Chatbot</label>}</button>
         
-        {console.log(chatBotTexts)}
       </div>
 
       <div className="p-3 border-t border-neutral-300 dark:border-neutral-700">
         <form onSubmit={(e) => {
              e.preventDefault();
              if(isUser && (text !== "")){
-              setUserTexts([...userTexts, text]);
-              setAllTexts([...allTexts, [2, text]]);
+              const userMessage = [2, text];
+
+              setAllTexts(prev => [...prev, userMessage]);
+
+              fetch("http://127.0.0.1:5000/chat", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ prompt: text })
+              })
+              .then(res => res.json())
+              .then(data => {
+                const botMessage = [1, data.response];
+                setAllTexts(prev => [...prev, botMessage]);
+
+              });
+              
+
              }
-             else{
-              setChatBotTexts([...chatBotTexts, text]);
-              setAllTexts([...allTexts, [1, text]]);
-             }
-             
              
              setText("");
           }}>
