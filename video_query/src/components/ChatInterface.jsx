967abf1e53b2c46cb951cd3b809cea8cc8755765
Gaurav <gauravdharmadhikari2@gmail.com>
2025-07-02 12:22:47 -0700

    implemented texting functionality for chatbot and user

--- video_query/src/components/ChatInterface.jsx
+++ video_query/src/components/ChatInterface.jsx
@@ -2,8 +2,11 @@ import { useState } from "react"
 import Conversation from "./Conversation"
 
 const ChatInterface = () => {
+  const [allTexts, setAllTexts] = useState([[1, "What can I help with?"]])
+  const [chatBotTexts, setChatBotTexts] = useState(["What can I help with?"])
   const [userTexts, setUserTexts] = useState([])
   const [text, setText] = useState("")
+  const [isUser, setIsUser] = useState(false)
 
   return (
     <div className="font-inter w-[360px] h-[500px] box-border border border-white overflow-hidden flex flex-col bg-white dark:bg-neutral-800 rounded-2xl shadow-[0_0_20px_rgba(0,0,0,0.5)]">
@@ -12,13 +15,28 @@ const ChatInterface = () => {
       </div>
 
       <div className="flex-grow overflow-y-auto px-3">
-        <Conversation userTexts={userTexts}/>
+        <Conversation chatBotTexts={chatBotTexts} userTexts={userTexts} allTexts={allTexts}/>
+      </div>
+
+      <div>
+        <button onClick={() => {setIsUser(!isUser)}}>Toggle: {isUser ? <label>User</label> : <label>Chatbot</label>}</button>
+        
+        {console.log(chatBotTexts)}
       </div>
 
       <div className="p-3 border-t border-neutral-300 dark:border-neutral-700">
         <form onSubmit={(e) => {
              e.preventDefault();
-             setUserTexts([...userTexts, {text}]);
+             if(isUser && (text !== "")){
+              setUserTexts([...userTexts, text]);
+              setAllTexts([...allTexts, [2, text]]);
+             }
+             else{
+              setChatBotTexts([...chatBotTexts, text]);
+              setAllTexts([...allTexts, [1, text]]);
+             }
+             
+             
              setText("");
           }}>
             <input
--- video_query/src/components/Conversation.jsx
+++ video_query/src/components/Conversation.jsx
@@ -1,14 +1,18 @@
-const Conversation = ({userTexts}) => {
+const Conversation = ({chatBotTexts, userTexts, allTexts}) => {
   return (
     <div className="flex flex-col items-end space-y-4">
-      <div className="text-left self-start bg-neutral-100 dark:bg-neutral-200 text-white dark:text-black px-4 py-2 max-w-[75%] rounded-2xl break-words">
-        What can I help with?
-      </div>
-      {userTexts.map(({text}) => (
-        <div className="text-left self-end bg-neutral-300 dark:bg-neutral-700 text-black dark:text-white px-4 py-2 max-w-[75%] rounded-2xl break-words">
-          {text}
-        </div>
+      {allTexts.map((text, idx) => (
+        text[0] === 1 ? (
+          <div key={idx} className="text-left self-start bg-neutral-100 dark:bg-neutral-200 text-white dark:text-black px-4 py-2 max-w-[75%] rounded-2xl break-words">
+            {text[1]}
+          </div>
+        ) : (
+          <div key={idx} className="text-left self-end bg-neutral-300 dark:bg-neutral-700 text-black dark:text-white px-4 py-2 max-w-[75%] rounded-2xl break-words">
+            {text[1]}
+          </div>
+        )
       ))}
+
       
     </div>
   )
