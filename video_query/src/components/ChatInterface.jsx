c61b9d56db753a28ec2a3904111a0741c237da13
Gaurav <gauravdharmadhikari2@gmail.com>
2025-07-04 00:47:22 -0700

    updated model & UI

--- video_query/src/components/ChatInterface.jsx
+++ video_query/src/components/ChatInterface.jsx
@@ -1,10 +1,11 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import Conversation from "./Conversation"
 
 const ChatInterface = () => {
   const [allTexts, setAllTexts] = useState([[1, "What can I help with?"]])
   const [text, setText] = useState("")
   const [currentUrl, setCurrentUrl] = useState("https://www.youtube.com/watch?v=9hE5-98ZeCg");
+  const chatRef = useRef(null);
 
   useEffect(() => {
     const handleMessage = (event) => {
@@ -18,6 +19,14 @@ const ChatInterface = () => {
     return () => window.removeEventListener("message", handleMessage);
   }, []);
 
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTo({
+        top: chatRef.current.scrollHeight,
+        behavior: "smooth"
+      });
+    }
+  }, [allTexts]); 
 
   return (
     <div className="font-inter w-[360px] h-[500px] box-border border border-white overflow-hidden flex flex-col bg-white dark:bg-neutral-800 rounded-2xl shadow-[0_0_20px_rgba(0,0,0,0.5)]">
@@ -26,12 +35,10 @@ const ChatInterface = () => {
       </div>
       
       <div className="flex-grow overflow-y-auto px-3">
-        <Conversation allTexts={allTexts}/>
+        <Conversation chatRef={chatRef} allTexts={allTexts}/>
       </div>
 
-      <div>
-        
-      </div>
+      <div></div>
 
       <div className="p-3 border-t border-neutral-300 dark:border-neutral-700">
         <form onSubmit={(e) => {
@@ -39,6 +46,21 @@ const ChatInterface = () => {
               const userMessage = [2, text];
 
               setAllTexts(prev => [...prev, userMessage]);
+              setAllTexts(prev => [...prev, [1, 
+
+                <div role="status flex flex-row px-2">
+                    <svg aria-hidden="true" class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg>
+                    
+                    <div>
+                      Thinking...
+                    </div>
+                    
+                </div>
+
+              ]]);
 
               fetch("https://video-query-reew.onrender.com/chat", {
                 method: "POST",
@@ -52,7 +74,10 @@ const ChatInterface = () => {
               .then(data => {
                 console.log(currentUrl)
                 const botMessage = [1, data.response];
-                setAllTexts(prev => [...prev, botMessage]);
+                setAllTexts(prev => [
+                  ...prev.slice(0, -1), 
+                  botMessage
+                ]);
 
               });
               
--- video_query/src/components/Conversation.jsx
+++ video_query/src/components/Conversation.jsx
@@ -1,6 +1,6 @@
-const Conversation = ({allTexts}) => {
+const Conversation = ({allTexts, chatRef}) => {
   return (
-    <div className="flex flex-col items-end space-y-4">
+    <div ref={chatRef} className="flex flex-col items-end space-y-4 pb-24 ">
       {allTexts.map((text, idx) => (
         text[0] === 1 ? (
           <div key={idx} className="text-left self-start bg-neutral-100 dark:bg-neutral-200 text-white dark:text-black px-4 py-2 max-w-[75%] rounded-2xl break-words">
